a70aa84dce0006dc7377392aa5a1e3f63b9f00ec
rkturtles <rkturtles@outlook.com>
2016-06-10 05:19:37 +0000

    sales navigator support

--- run.js
+++ run.js
@@ -11,7 +11,7 @@ var text_visiting_extracting = '(Visiting & Extracting)';
 var text_next_to_visit = '(Next To Visit)';
 
 var color_done = '#39D5FF';
-var color_already_done = '#F6C4A3';
+var color_already_done = '#E7E7E7';
 var color_visiting_extracting = '#5EFAF7';
 var color_next_to_visit = '#FFCCBC';
 
@@ -19,11 +19,11 @@ function start() {
 	stopping = false;
 	$('#extractor-start-button').text('Stop');
 	stoppingAfter = parseInt($('#extractor-stopping-number').val()) || 0;
-	
+
 	$('#extractor-stopping-after-number').text(stoppingAfter);
 
 	$('#extractor-stopping').fadeIn();
-	startVisiting(0);	
+	startVisiting(0);
 }
 
 function stop() {
@@ -31,111 +31,113 @@ function stop() {
 	$('#extractor-start-button').text('Start');
 	$('#extractor-stopping').hide();
 
-	for(var i = 0; i < intervals.length; i++) {
+	for (var i = 0; i < intervals.length; i++) {
 		clearInterval(intervals[i]);
 	}
 }
 
 function startVisiting(i) {
-		if(stopping) {
-			stop();
+	if (stopping) {
+		stop();
+		return;
+	}
+
+	var peopleRows = $('#results .people');
+	var personRow = peopleRows[i];
+
+	var personTitle = $(personRow).find('a.title');
+
+	if (personRow && personTitle) {
+		var nowText = $(personTitle).text();
+		var personLink = $(personTitle).attr('href');
+
+		i++;
+
+		//dont visit again if already visited
+		if (!personLink || nowText.indexOf('LinkedIn Member') > -1 || nowText.indexOf(
+				text_next_to_visit) > -1 || nowText.indexOf(text_done) > -1 || nowText.indexOf(
+				text_already_done) > -1) {
+
+			if (nowText.indexOf(text_already_done) === -1) {
+				nowText = nowText.replace(' ' + text_done, '').replace(' ' +
+					text_next_to_visit, '');
+				$(personTitle).text(nowText + ' ' + text_already_done);
+				$(personRow).css('background-color', color_already_done);
+			}
+
+			startVisiting(i);
 			return;
 		}
 
-		var peopleRows = $('#results .people');
-		var personRow = peopleRows[i];
-		
-		var personTitle = $(personRow).find('a.title');
-		
-		if(personRow && personTitle) {
-			var nowText = $(personTitle).text();
-			var personLink = $(personTitle).attr('href');	
-			
-			i++;
 
-			//dont visit again if already visited
-			if(!personLink || nowText.indexOf('LinkedIn Member') > -1 || nowText.indexOf(text_next_to_visit) > -1 || nowText.indexOf(text_done) > -1 || nowText.indexOf(text_already_done) > -1) {
+		nowText += " " + text_next_to_visit;
+		$(personRow).css('background-color', color_next_to_visit);
+		$(personTitle).text(nowText);
 
-				if(nowText.indexOf(text_already_done) === -1) {
-					nowText = nowText.replace(' ' + text_done, '').replace(' ' + text_next_to_visit, '');
-					$(personTitle).text(nowText + ' ' + text_already_done);
-					$(personRow).css('background-color', color_already_done);
-				}
+		var delay = Math.round(Math.random() * (25000 - 10000)) + 10000;
+		var delayS = Math.round(delay / 1000);
 
-				startVisiting(i);
-				return;
-			}
-			
+		$('#extractor-next-visit').text(delayS + 's');
 
-			nowText += " " + text_next_to_visit;
-			$(personRow).css('background-color', color_next_to_visit);
-			$(personTitle).text(nowText);
-			
-			var delay = Math.round(Math.random() * (25000 - 10000)) + 10000;								
-			var delayS = Math.round(delay/1000);
+		var interval = setInterval(function() {
+			delayS--;
 
 			$('#extractor-next-visit').text(delayS + 's');
 
-			var interval = setInterval(function() {
-				delayS--;
-				
-				$('#extractor-next-visit').text(delayS + 's');
+			if (delayS <= 0) {
 
-				if(delayS <= 0) {
-		
-					var intervalIndex = intervals.indexOf(interval);
-					if(intervalIndex > -1)
-					{
-						clearInterval(intervals[intervalIndex]);
-						intervals.splice(intervalIndex, 1);
-					}
-						
-					if(nowText.indexOf(text_visiting_extracting) === -1) {
-						nowText = nowText.replace(text_next_to_visit, text_visiting_extracting);
-						$(personTitle).text(nowText);
-						$(personRow).css('background-color', color_visiting_extracting);
-					}		
-
-					visitPerson(personLink, function(profileDetails) {
-						saveOrPrint(profileDetails);
-						
-						incrementVisitCount();
-	
-						if(nowText.indexOf(text_done) === -1) {
-							nowText = nowText.replace(text_visiting_extracting, text_done);
-							$(personTitle).text(nowText);
-							$(personRow).css('background-color', color_done);
-						}
-
-						stoppingAfter--;
-						$('#extractor-stopping-after-number').text(stoppingAfter);
-						
-						if(stoppingAfter <= 0) {
-							stop();	
-						} else {
-							startVisiting(i);
-						}
-
-						return;
-					});
+				var intervalIndex = intervals.indexOf(interval);
+				if (intervalIndex > -1) {
+					clearInterval(intervals[intervalIndex]);
+					intervals.splice(intervalIndex, 1);
 				}
-			}, 1000 );
-			
-			intervals.push(interval);
 
-		} else {
-			var next = $('a.page-link[rel="next"]');
+				if (nowText.indexOf(text_visiting_extracting) === -1) {
+					nowText = nowText.replace(text_next_to_visit, text_visiting_extracting);
+					$(personTitle).text(nowText);
+					$(personRow).css('background-color', color_visiting_extracting);
+				}
 
-			if(next) { 
-				$('a.page-link[rel="next"]')[0].click();
-				
-				setTimeout(function() { 
-					startVisiting(0);
-				}, 4000);
-			} else {
-				stop();
+				visitPerson(personLink, function(profileDetails) {
+					saveOrPrint(profileDetails);
+
+					incrementVisitCount();
+
+					if (nowText.indexOf(text_done) === -1) {
+						nowText = nowText.replace(text_visiting_extracting, text_done);
+						$(personTitle).text(nowText);
+						$(personRow).css('background-color', color_done);
+					}
+
+					stoppingAfter--;
+					$('#extractor-stopping-after-number').text(stoppingAfter);
+
+					if (stoppingAfter <= 0) {
+						stop();
+					} else {
+						startVisiting(i);
+					}
+
+					return;
+				});
 			}
+		}, 1000);
+
+		intervals.push(interval);
+
+	} else {
+		var next = $('a.page-link[rel="next"]');
+
+		if (next) {
+			$('a.page-link[rel="next"]')[0].click();
+
+			setTimeout(function() {
+				startVisiting(0);
+			}, 4000);
+		} else {
+			stop();
 		}
+	}
 }
 
 function visitPerson(link, completed) {
@@ -151,27 +153,30 @@ function visitPerson(link, completed) {
 
 		var name = $($(html).find('.full-name')).text();
 		var headline = $($(html).find('#headline')).text();
-		var title = $($(html).find('#background-experience .current-position header h4')).map(function() {
-				return $(this).text();
-			}).get().join(' ,');
+		var title = $($(html).find(
+			'#background-experience .current-position header h4')).map(function() {
+			return $(this).text();
+		}).get().join(' ,');
 
 		var locality = $($(html).find('#top-card .locality')).text();
 		var industry = $($(html).find('#top-card .industry')).text();
 		var currentCompanies = $($(html).find('#overview-summary-current ol')).text();
 		var previousCompanies = $($(html).find('#overview-summary-past ol')).text();
-		var lastCompany = $($(html).find('#overview-summary-current a[href*="/company/"]:first'));
+		var lastCompany = $($(html).find(
+			'#overview-summary-current a[href*="/company/"]:first'));
 
 		var lastCompanyLink = '';
-		if(lastCompany && lastCompany.attr('href')) {
+		if (lastCompany && lastCompany.attr('href')) {
 			lastCompanyLink = 'https://www.linkedin.com' + lastCompany.attr('href');
 		}
 
 		var education = $($(html).find('#overview-summary-education ol')).text();
 
 		var connections = $($(html).find('.member-connections > strong')).text();
-		var skills = $($(html).find('#profile-skills .endorse-item-name-text')).map(function() {
-			return $(this).text();
-		}).get().join(', ');
+		var skills = $($(html).find('#profile-skills .endorse-item-name-text')).map(
+			function() {
+				return $(this).text();
+			}).get().join(' ,');
 
 		var interests = $($(html).find('#interests-view li')).text();
 
@@ -195,54 +200,58 @@ function visitPerson(link, completed) {
 			link: link,
 			companyWebsite: companyWebsite
 		};
-	
-		if(!lastCompanyLink) {
+
+		if (!lastCompanyLink) {
 			completed(profileDetails);
 			return;
 		} else {
 			$.ajax({
-				url : lastCompanyLink,
+				url: lastCompanyLink,
 				type: 'get',
-				timeout: 10000 
+				timeout: 10000
 			}).done(function(data) {
 				var htmlAgain = $.parseHTML(data);
-				var code = $($(htmlAgain).find('#stream-about-section-embed-id-content')).html() || '{}';	
+				var code = $($(htmlAgain).find('#stream-about-section-embed-id-content'))
+					.html() || '{}';
 				var str = code.replace("<!--", "").replace("-->", "");
 				var companyWebsite = JSON.parse(str).website;
-				
-				if(companyWebsite)
+
+				if (companyWebsite)
 					profileDetails.companyWebsite = companyWebsite.toLowerCase().trim();
 
 				completed(profileDetails);
 				return;
-			});		
+			});
 		}
-	
+
 	});
 }
 
 function incrementVisitCount() {
-		var day = savedDetails.day || 0;
-		var visited = savedDetails.visited || 0;
-		
-		var nowDay = new Date().getDate();
-		if(day != nowDay) {
-			visited = 0;
-			savedDetails.day = nowDay;
-			savedDetails.visited = 0;
-		}
+	var day = savedDetails.day || 0;
+	var visited = savedDetails.visited || 0;
 
-		visited++;
+	var nowDay = new Date().getDate();
+	if (day != nowDay) {
+		visited = 0;
+		savedDetails.day = nowDay;
+		savedDetails.visited = 0;
+	}
 
-		savedDetails.visited = visited;
-		$('#extractor-visited').text(visited);
-		chrome.runtime.sendMessage({'message' : 'save', 'toSaveDetails' : savedDetails}, function() {});
+	visited++;
+
+	savedDetails.visited = visited;
+	$('#extractor-visited').text(visited);
+	chrome.runtime.sendMessage({
+		'message': 'save',
+		'toSaveDetails': savedDetails
+	}, function() {});
 }
 
 function saveOrPrint(details) {
 	var API = savedDetails.API;
 
-	if(API) {
+	if (API) {
 		try {
 			$.ajax({
 				url: API,
@@ -251,96 +260,84 @@ function saveOrPrint(details) {
 					data: details
 				}
 			}).complete(function(returned) {
-				
+
 			});
-		} catch(e) {}
+		} catch (e) {}
 
 	}
- 
+
 	console.log(details);
 }
 
 function initialize(complete) {
-	chrome.runtime.sendMessage({'message' : 'load'}, function(returnedDetails) {
-		if(returnedDetails)
+	chrome.runtime.sendMessage({
+		'message': 'load'
+	}, function(returnedDetails) {
+		if (returnedDetails)
 			savedDetails = returnedDetails;
 
 		$.get(chrome.extension.getURL("toolbar.html"), function(toolbarHTML) {
 			$('#srp_main_').append(toolbarHTML);
-		
-			if(API)
-			{
+
+			if (API) {
 				$('#extractor-save-to').val(API);
 			}
-			
+
 			var API = savedDetails.API || '';
-					
+
 			var day = savedDetails.day || 0;
 			var visited = savedDetails.visited || 0;
-			
+
 			var nowDay = new Date().getDate();
-			if(day != nowDay) {
+			if (day != nowDay) {
 				visited = 0;
 				savedDetails.day = nowDay;
 				savedDetails.visited = 0;
-				chrome.runtime.sendMessage({'message' : 'save', 'toSaveDetails' : savedDetails}, function() {});
-			}		
-	
+				chrome.runtime.sendMessage({
+					'message': 'save',
+					'toSaveDetails': savedDetails
+				}, function() {});
+			}
+
 			$('#extractor-visited').text(visited);
 
 			complete();
 		});
 	});
-	
+
 }
 
 $(function() {
 	$('#srp_main_').on('click', '#extractor-start-button', function() {
 		var text = $(this).text();
-		
-		if(text === 'Start') {
+
+		if (text === 'Start') {
 			start();
 		} else {
 			stop();
-		}	
-	});
-	
-	$('#srp_main_').on('click', '#extractor-change-save', function() {
-		var text = $(this).text();
-		var self = this;
-		
-		var API = $('#extractor-save-to').val();
-		
-		if(API) {
-			savedDetails.API = API;
-			chrome.runtime.sendMessage({'message' : 'save', 'toSaveDetails' : savedDetails}, function() {});	
-			$(this).text("Saved!").attr('disabled', true);	
-		} else 
-			$(this).text("Invalid API Endpoint").attr('disabled', true);
-
-		setTimeout(function() {
-			$(self).text(text).attr('disabled', false);			
-		}, 2000);
+		}
 	});
 
 	$('#srp_main_').on('click', '#extractor-options', function() {
-		chrome.runtime.sendMessage({'message' : 'options'}, function() {});
+		chrome.runtime.sendMessage({
+			'message': 'options'
+		}, function() {});
 	});
 
 	$('#srp_main_').on('change', '#extractor-stopping-number', function() {
 		var max = parseInt($(this).attr('max'));
 		var min = parseInt($(this).attr('min'));
 
-		if($(this).val() > max) {
+		if ($(this).val() > max) {
 			$(this).val(max);
-		} else if($(this).val() < min) {
+		} else if ($(this).val() < min) {
 			$(this).val(min);
 		}
 	});
 
 	initialize(function() {
 		extend();
-	});	
+	});
 });
 
 //add more functions
---
+++ runS.js
@@ -0,0 +1,330 @@
+var stopping = false;
+var intervals = [];
+
+var savedDetails = {};
+
+var stoppingAfter = 0;
+
+var text_done = '(Done)';
+var text_already_done = '(Skipped - Already Done)';
+var text_visiting_extracting = '(Visiting & Extracting)';
+var text_next_to_visit = '(Next To Visit)';
+
+var color_done = '#39D5FF';
+var color_already_done = '#E7E7E7';
+var color_visiting_extracting = '#5EFAF7';
+var color_next_to_visit = '#FFCCBC';
+
+function start() {
+  stopping = false;
+  $('#extractor-start-button').text('Stop');
+  stoppingAfter = parseInt($('#extractor-stopping-number').val()) || 0;
+
+  $('#extractor-stopping-after-number').text(stoppingAfter);
+
+  $('#extractor-stopping').fadeIn();
+  startVisiting(0);
+}
+
+function stop() {
+  stopping = true;
+  $('#extractor-start-button').text('Start');
+  $('#extractor-stopping').hide();
+
+  for (var i = 0; i < intervals.length; i++) {
+    clearInterval(intervals[i]);
+  }
+}
+
+function startVisiting(i) {
+  if (stopping) {
+    stop();
+    return;
+  }
+
+  var peopleRows = $('.spotlights-result').parent();
+  var personRow = peopleRows[i];
+
+  var personTitle = $(personRow).find('.name > a');
+
+  if (personRow && personTitle) {
+    var nowText = $(personTitle).text();
+    var personLink = $(personTitle).attr('href');
+
+    i++;
+
+    //dont visit again if already visited
+    // if (!personLink || personLink.indexOf('/sales/profile') === -1 || nowText.indexOf(
+    //     'LinkedIn Member') > -1 || nowText.indexOf(
+
+    if (!personLink || personLink.indexOf('/sales/profile') === -1 || nowText.indexOf(
+        text_next_to_visit) > -1 || nowText.indexOf(text_done) > -1 ||
+      nowText.indexOf(
+        text_already_done) > -1) {
+
+      if (nowText.indexOf(text_already_done) === -1) {
+        nowText = nowText.replace(' ' + text_done, '').replace(' ' +
+          text_next_to_visit, '');
+        $(personTitle).text(nowText + ' ' + text_already_done);
+        $(personRow).css('background-color', color_already_done);
+      }
+
+      startVisiting(i);
+      return;
+    }
+
+
+    nowText += " " + text_next_to_visit;
+    $(personRow).css(
+      'background-color', color_next_to_visit);
+    $(personTitle).text(nowText);
+
+    var delay = Math.round(Math.random() * (25000 - 10000)) + 10000;
+    var delayS = Math.round(delay / 1000);
+
+    $('#extractor-next-visit').text(delayS + 's');
+
+    var interval = setInterval(function() {
+      delayS--;
+
+      $('#extractor-next-visit').text(delayS + 's');
+
+      if (delayS <= 0) {
+
+        var intervalIndex = intervals.indexOf(interval);
+        if (intervalIndex > -1) {
+          clearInterval(intervals[intervalIndex]);
+          intervals.splice(intervalIndex, 1);
+        }
+
+        if (nowText.indexOf(text_visiting_extracting) === -1) {
+          nowText = nowText.replace(text_next_to_visit,
+            text_visiting_extracting);
+          $(personTitle).text(nowText);
+          $(personRow).css('background-color',
+            color_visiting_extracting);
+        }
+
+        visitPerson(personLink, function(profileDetails) {
+          saveOrPrint(profileDetails);
+
+          incrementVisitCount();
+
+          if (nowText.indexOf(text_done) === -1) {
+            nowText = nowText.replace(text_visiting_extracting,
+              text_done);
+            $(personTitle).text(nowText);
+            $(personRow).css('background-color', color_done);
+          }
+
+          stoppingAfter--;
+          $('#extractor-stopping-after-number').text(stoppingAfter);
+
+          if (stoppingAfter <= 0) {
+            stop();
+          } else {
+            startVisiting(i);
+          }
+
+          return;
+        });
+      }
+    }, 1000);
+
+    intervals.push(interval);
+
+  } else {
+    var next = $('a.page-link[title*="Next"]');
+
+    if (next && next.length) {
+      $('a.page-link[title*="Next"]')[0].click();
+
+      setTimeout(function() {
+        startVisiting(0);
+      }, 4000);
+    } else {
+      stop();
+    }
+  }
+}
+
+function visitPerson(link, completed) {
+  $.ajax({
+    url: link,
+    type: 'get',
+    timeout: 10000
+  }).done(function(data) {
+    var html = $.parseHTML(data);
+
+    var embeddedJson = $(html).filter('#embedded-json');
+    var jsonStr = '{}';
+
+    if (embeddedJson) {
+      jsonStr = embeddedJson.html() || '{}';
+    }
+
+    jsonStr = jsonStr.replace("<!--", "").replace("-->", "");
+    var profileDetails = JSON.parse(jsonStr);
+
+    var positions = ((profileDetails.positionsView || {}).positions) || [];
+    var lastPosition = positions[0] || {};
+    var lastCompanyLink = lastPosition.companyUrl;
+
+    if (!lastCompanyLink) {
+      completed(profileDetails);
+      return;
+    } else {
+      lastCompanyLink = window.location.protocol + lastCompanyLink.replace(
+        /^https?:/, '');
+
+      $.ajax({
+        url: lastCompanyLink,
+        type: 'get',
+        timeout: 10000
+      }).done(function(data) {
+        var htmlAgain = $.parseHTML(data);
+        var companyJson = $(htmlAgain).find("code").last();
+        var companyJsonStr = '{}';
+
+        if (companyJson) {
+          companyJsonStr = companyJson.html() || '{}';
+        }
+
+        companyJsonStr = companyJsonStr.replace("<!--", "").replace(
+          "-->", "");
+        var companyDetails = JSON.parse(companyJsonStr);
+        var companyWebsite = ((companyDetails.account || {}).website ||
+          '');
+
+        if (companyWebsite)
+          profileDetails.companyWebsite = companyWebsite.toLowerCase()
+          .trim();
+
+        completed(profileDetails);
+        return;
+      });
+    }
+
+  });
+}
+
+function incrementVisitCount() {
+  var day = savedDetails.day || 0;
+  var visited = savedDetails.visited || 0;
+
+  var nowDay = new Date().getDate();
+  if (day != nowDay) {
+    visited = 0;
+    savedDetails.day = nowDay;
+    savedDetails.visited = 0;
+  }
+
+  visited++;
+
+  savedDetails.visited = visited;
+  $('#extractor-visited').text(visited);
+  chrome.runtime.sendMessage({
+    'message': 'save',
+    'toSaveDetails': savedDetails
+  }, function() {});
+}
+
+function saveOrPrint(details) {
+  var API = savedDetails.API;
+
+  if (API) {
+    try {
+      $.ajax({
+        url: API,
+        type: 'POST',
+        data: {
+          data: details
+        }
+      }).complete(function(returned) {
+
+      });
+    } catch (e) {}
+
+  }
+
+  console.log(details);
+}
+
+function initialize(complete) {
+  chrome.runtime.sendMessage({
+    'message': 'load'
+  }, function(returnedDetails) {
+    if (returnedDetails)
+      savedDetails = returnedDetails;
+
+    $.get(chrome.extension.getURL("toolbar.html"), function(toolbarHTML) {
+      $('#stream-container').append(toolbarHTML);
+
+      if (API) {
+        $('#extractor-save-to').val(API);
+      }
+
+      var API = savedDetails.API || '';
+
+      var day = savedDetails.day || 0;
+      var visited = savedDetails.visited || 0;
+
+      var nowDay = new Date().getDate();
+      if (day != nowDay) {
+        visited = 0;
+        savedDetails.day = nowDay;
+        savedDetails.visited = 0;
+        chrome.runtime.sendMessage({
+          'message': 'save',
+          'toSaveDetails': savedDetails
+        }, function() {});
+      }
+
+      $('#extractor-visited').text(visited);
+
+      complete();
+    });
+  });
+
+}
+
+$(function() {
+  $('#stream-container').on('click', '#extractor-start-button',
+    function() {
+      var text = $(this).text();
+
+      if (text === 'Start') {
+        start();
+      } else {
+        stop();
+      }
+    });
+
+  $('#stream-container').on('click', '#extractor-options', function() {
+    chrome.runtime.sendMessage({
+      'message': 'options'
+    }, function() {});
+  });
+
+  $('#stream-container').on('change', '#extractor-stopping-number',
+    function() {
+      var max = parseInt($(this).attr('max'));
+      var min = parseInt($(this).attr('min'));
+
+      if ($(this).val() > max) {
+        $(this).val(max);
+      } else if ($(this).val() < min) {
+        $(this).val(min);
+      }
+    });
+
+  initialize(function() {
+    extend();
+  });
+});
+
+//add more functions
+function extend() {
+
+
+}
